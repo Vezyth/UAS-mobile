80b77054faf413d740672af5cd8f8f6c8eb64480
Adam Christoper <adam2510025@gmail.com>
2023-01-02 22:44:10 +0700

    login & register

--- App.js
+++ App.js
@@ -1,26 +1,29 @@
-import { View } from "react-native"
-import { NavigationContainer } from "@react-navigation/native"
-import { createNativeStackNavigator } from "@react-navigation/native-stack" 
+import { View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import Home from "./src/components/Home"
-import Detail from "./src/components/Details"
+import Home from "./src/components/Home";
+import Detail from "./src/components/Details";
+import Login from "./src/components/Login";
+import Register from "./src/components/Register";
 
 const App = () => {
-
-  const Stack = createNativeStackNavigator()
+  const Stack = createNativeStackNavigator();
 
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{
-        headerShown:false
-      }}>
-        <Stack.Screen name="home" component={Home}/>
-        <Stack.Screen name="details" component={Detail}/>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name="login" component={Login} />
+        <Stack.Screen name="register" component={Register} />
+        <Stack.Screen name="home" component={Home} />
+        <Stack.Screen name="details" component={Detail} />
       </Stack.Navigator>
     </NavigationContainer>
-  )
-}
+  );
+};
 
-export default App
-
-
+export default App;
